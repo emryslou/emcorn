9580cc3eac5a75f4962ef17ad02a32cfeb336897
eml <1065873330@qq.com>
2023-11-02 11:19:47 +0800

    send error non blocking

--- emcorn/worker.py
+++ emcorn/worker.py
@@ -12,7 +12,7 @@ import threading
 from emcorn import http
 from emcorn.logging import log
 from emcorn.http.request import RequestError
-from emcorn.util import import_app
+from emcorn.util import import_app, write_nonblock
 
 class Worker(object):
     signals = map(
@@ -97,8 +97,7 @@ class Worker(object):
             result = self.app(req.read(), req.start_response)
             http.HttpResponse(conn, result, req).send()
         except Exception as exc:
-            conn.send(b'HTTP/1.1 500 Internal Server Error\r\n\r\n')
-            conn.close()
+            write_nonblock(conn, b'HTTP/1.1 500 Internal Server Error\r\n\r\n')
             log.error(f'{client}:request error {exc}')
             import traceback
             traceback.print_exc()
