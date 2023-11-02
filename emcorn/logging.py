630621054a2bfe71e59c20d9fe37829d6a5df763
eml <1065873330@qq.com>
2023-11-02 11:52:16 +0800

    log config

--- emcorn/logging.py
+++ emcorn/logging.py
@@ -1,8 +1,20 @@
 import logging
 
 __all__ = [
-    'log',
+    'log', 'configure'
 ]
 
 logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s][%(levelname)s]:%(message)s')
 log = logging.getLogger('emcorn')
+
+def configure(opts):
+    handlers = []
+    if opts.logfile != '-':
+        handlers.append(logging.FileHandler(opts.logfile))
+    
+    loglevel = logging.getLevelName(opts.loglevel.upper())
+
+    log.setLevel(loglevel)
+    for h in handlers:
+        h.setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s]:%(message)s'))
+        log.addHandler(h)
--- emcorn/main.py
+++ emcorn/main.py
@@ -1,7 +1,8 @@
+import logging
 import optparse as op
 
 from emcorn.arbiter import Arbiter
-from emcorn.logging import log
+from emcorn.logging import log, configure as configure_log
 from emcorn.util import import_app
 
 def options():
@@ -9,16 +10,36 @@ def options():
         op.make_option('--host', dest='host', default='127.0.0.1', help='监听IP，默认: [%default]'),
         op.make_option('--port', dest='port', default='8089', type='int', help='监听端口，默认: [%default]'),
         op.make_option('--workers', dest='workers', default='1', type='int', help='开启工作进程数，默认: [%default]'),
+        op.make_option('--log-level', dest='loglevel', default='info', help='日志输出级别[debug, info, warn, error, fatal]: [%default]'),
+        op.make_option('--log-file', dest='logfile', default='-', help='日志输出文件，默认: [%default]'),
     ]
 
 
 def main(usage):
     parser = op.OptionParser(usage=usage, option_list=options())
     opts, args = parser.parse_args()
-    app = import_app(args[0])
+    configure_log(opts)
 
-    arbiter = Arbiter((opts.host, opts.port), opts.workers, app)
-    log.info('Emcorn starting ...')
+    print(emcorn_log())
     log.info(f'listening: {opts.host}:{opts.port}')
     log.info(f'worker count:{opts.workers}')
+
+    app = import_app(args[0])
+    arbiter = Arbiter((opts.host, opts.port), opts.workers, app)
     arbiter.run()
+
+
+def emcorn_log():
+    return """
+
+ /$$$$$$$$                                                      
+| $$_____/                                                      
+| $$       /$$$$$$/$$$$   /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$$ 
+| $$$$$   | $$_  $$_  $$ /$$_____/ /$$__  $$ /$$__  $$| $$__  $$
+| $$__/   | $$ \ $$ \ $$| $$      | $$  \ $$| $$  \__/| $$  \ $$
+| $$      | $$ | $$ | $$| $$      | $$  | $$| $$      | $$  | $$
+| $$$$$$$$| $$ | $$ | $$|  $$$$$$$|  $$$$$$/| $$      | $$  | $$
+|________/|__/ |__/ |__/ \_______/ \______/ |__/      |__/  |__/
+                                                                
+                                                                
+    """
